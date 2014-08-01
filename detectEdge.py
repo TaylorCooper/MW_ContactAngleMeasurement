d4de6f693d32f48da8d8cfaf0f42cdccacf0b4f4
TaylorCooper <cooper.taylor@gmail.com>
2014-08-01 15:22:45 -0700

    Got filtering working for videos.
    Going to remove a lot of non critical functionality from the script to
    make it less confusing.

--- detectEdge.py
+++ detectEdge.py
@@ -13,7 +13,7 @@ import os
 import string
 from pylab import *
 
-MAX_VIDEO_SIZE = 100 #Frames
+MAX_VIDEO_SIZE = 1000 #Frames
 
 def getContactAngle(inputImg, outPath, ident, roi, debug=True):
     """Estimates contact angle using hough lines.
@@ -34,9 +34,11 @@ def getContactAngle(inputImg, outPath, ident, roi, debug=True):
     else:
         img = inputImg
         
-    img = img[roi[0]:roi[1],roi[2]:roi[3]]
+    img = img[roi[0]:roi[1],roi[2]:roi[3]]  # vert:vert, hor:hor
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(gray,30,70,apertureSize = 3)
+    
+    # image, threshold1, threshold2, apertureSize
+    edges = cv2.Canny(gray,65,180,apertureSize = 3)
 
     # Save debugging images
     if debug:
@@ -46,7 +48,7 @@ def getContactAngle(inputImg, outPath, ident, roi, debug=True):
         cv2.imwrite(roiPath,img)
         cv2.imwrite(cannyPath,edges)
     
-    lines = cv2.HoughLines(edges,2,np.pi/1080,20)
+    lines = cv2.HoughLines(edges,2,np.pi/1080,30)
     
     if lines == None:
         print "houghLines dropout:", ident
@@ -115,27 +117,6 @@ def videoToImages(videoPath, outPath):
         
         count += 1
 
-def measureMultipleImages(inputPath, outputPath):
-    """ Use os.walk to measure all images in the folder
-    """
-
-    contactAngles = []
-    
-    for (root, subFolders, files) in os.walk(inputPath):
-    
-        #Second condition forces this to run in only the index dir
-        for i, item in enumerate(files):
-            if '.png' in item:
-                print item
-                angle = getContactAngle(inputPath+"\\"+item, outputPath,
-                                        item[8:11])
-                if angle:
-                    contactAngles.append(angle)
-                
-    plot(arange(len(contactAngles)), contactAngles)
-    savefig("contactAngles.png")
-
-
     
 def videoToContactAngles(videoPath, outPath, N, roi):
     """Analyzes every Nth image in a video with getContactAngle()
@@ -152,11 +133,12 @@ def videoToContactAngles(videoPath, outPath, N, roi):
     cap = cv2.VideoCapture(videoPath)
     
     if N <= 0:
-        return "N must be an integer >0."
+        print "N must be an integer >0."
+        return None
     
     count = 0
     contactAngles = []
-    frameNumbers = []    
+    frameNumbers = []
     
     while True:
         ret, frame = cap.read()
@@ -169,8 +151,8 @@ def videoToContactAngles(videoPath, outPath, N, roi):
             
             angle = getContactAngle(frame, outPath, imgNum, roi)
             if angle:
-                    contactAngles.append(angle)
-                    frameNumbers.append(count)
+                contactAngles.append(angle)
+                frameNumbers.append(float(count))
                     
         if count > MAX_VIDEO_SIZE or ret == False:
             print count
@@ -178,10 +160,130 @@ def videoToContactAngles(videoPath, outPath, N, roi):
         
         count += 1
 
-    plot(frameNumbers, contactAngles)
-    savefig(outPath+"\\contactAngles.png")
+    print frameNumbers
+    print contactAngles
+    clf() # Not certain what is requiring me to clear figure
+    plot(arange(len(contactAngles)), contactAngles)
+    savefig(outPath+"\\contactAngles_001.png")
+
+
+def videoToFilteredVideo(videoPath, outPath, N, deg, roi):
+    """Filters and saves every Nth image in a video with getContactAngle()
+    
+    Input:
+    videoPath = a string, path of video to be analyzed
+    outPath = a string, working directory
+    N = a positive integer, images divisable by it will be analyzed
+    
+    Output:
+    A cropped, filtered, rotated image
+    """
+    
+    if N <= 0:
+        print "N must be an integer >0."
+        return None
+
+    count = 0
+    
+    # Get rotation matrix centered in roi
+    x_cent = int((roi[0]+roi[1])/2)
+    y_cent = int((roi[2]+roi[3])/2)
+    rot_Matrx = cv2.getRotationMatrix2D((x_cent,y_cent),deg,1)
+    print x_cent, y_cent, deg
+    print rot_Matrx
+    
+    # Open output video    
+    height = roi[1]-roi[0]
+    width = roi[3]-roi[2]
+    outName = outPath + '\\_filter.avi'
+    
+    # Output Filename, compression, fps, (frame_W, frame_H)
+    
+    # Possible compression formats: M J P G, P I M 1, I 4 2 0
+    # Translation: Mjpg, i dont know, uncompressed avi
+    video = cv2.VideoWriter(outName, cv2.cv.CV_FOURCC('P','I','M','1')
+                            ,25,(width,height))
+
+    # Start reading input video
+    cap = cv2.VideoCapture(videoPath)
+        
+    while True:
+        ret, frame = cap.read()
+        
+        # If frameNum/N=0 is returned do analysis
+        if ret and (count % N) == 0:          
+                        
+            imgNum = string.zfill(count,4)
             
-# Basic functionality            
+            rows,cols,channels = frame.shape            
+            print ret, imgNum, cols, rows, channels
+            
+            # Rotate and crop image based on Deg / Roi
+            img = cv2.warpAffine(frame,rot_Matrx,(cols,rows))            
+            img_Nom = img[roi[0]:roi[1],roi[2]:roi[3]]  # vert:vert, hor:hor         
+
+            
+            # Gray scale and apply a mask to remove unecessary points
+            img = cv2.cvtColor(img_Nom,cv2.COLOR_BGR2GRAY) # convert to GS
+            rows, cols = img.shape
+            mask = np.zeros([rows,cols], np.uint8)
+            mask[:,:90] = 255
+            mask[:,280:] = 255
+            img = cv2.add(img,mask)
+
+            # Adjust contrast... this is difficult to get right
+            img = cv2.add(img,90)
+            r,img = cv2.threshold(img,160,255,cv2.THRESH_BINARY)
+#            img = cv2.equalizeHist(img)
+            
+            # Convert Nom to grayscale so it can output in the same png
+            img_Nom = cv2.cvtColor(img_Nom,cv2.COLOR_BGR2GRAY) # convert to GS 
+            img_Debug = np.hstack((img,img_Nom)) # Side by side for contrast          
+
+            cv2.imwrite(outPath+'\\'+imgNum+'.png', img_Debug)
+
+            # Convert back to BGR for video codec
+            img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR) # convert to GS  
+            video.write(img)
+            
+        if count > MAX_VIDEO_SIZE or ret == False:
+            print count
+            break
+        
+        count += 1
+
+    cv2.destroyAllWindows()
+    video.release()
+    
+
+def createVideo(outPath, roi):
+    """ Use os.walk to do stuff
+    """
+    height = roi[1]-roi[0]
+    width = roi[3]-roi[2]
+    outName = outPath + '\\CompileVideo.avi'
+    
+    # Possible compression formats: M J P G, P I M 1, I 4 2 0
+    # Translation: Mjpg, i dont know, uncompressed avi
+    video = cv2.VideoWriter(outName, cv2.cv.CV_FOURCC('P','I','M','1'),
+                            25,(width,height))
+                            
+
+    for (root, subFolders, files) in os.walk(outPath):
+    
+        #Second condition forces this to run in only the index dir
+        for i, item in enumerate(files):
+            print item, root            
+            if '.png' in item:
+                img = cv2.imread(root+'\\'+item)                
+                video.write(img)
+
+    #cv2.destroyAllWindows()
+    video.release()
+
+###            
+### Basic functionality            
+###
 #path = "C:\\Users\Taylor\\Documents\\GitHub\\MW_ContactAngleMeasurement"
 #pathCA = path + "\\contact_angle"
 #pathHough = path + "\\houghoutputs"
@@ -190,7 +292,9 @@ def videoToContactAngles(videoPath, outPath, N, roi):
 #roi = (450,600,700,800)
 #measureMultipleImages(pathCA, pathHough, roi)
             
-# Basic Spliting video to images
+###
+### Basic Spliting video to images
+###
 #inputVid = "C:\\Users\\Taylor\\Documents\\GitHub\\MW_ContactAngleMeasurement\\\
 #drop\\drop.avi"
 #outputPath = "C:\\Users\\Taylor\\Documents\\GitHub\\MW_ContactAngleMeasurement\
@@ -199,23 +303,59 @@ def videoToContactAngles(videoPath, outPath, N, roi):
 #print outputPath
 #videoToImages(inputVid, outputPath)
 
-# Combined, split video and analyze it
-inputVid = "D:\\Dropbox\\Clients\\AFCC\\Projects\\Metal FSU\\\
-A2 - Material Characterization\\TEST DATA\TEST CA1\\20140725_GizmoDry_Analysis\
-\\2014_07_28\\20140728-14_30_42-CA1_M01SS-27.MOV"
-outputPath = "D:\\Dropbox\\Clients\\AFCC\\Projects\\Metal FSU\\\
-A2 - Material Characterization\\TEST DATA\TEST CA1\\20140725_GizmoDry_Analysis\
-\\2014_07_28\\work"
-roi = (200,1800,400,1050)
+###
+### Combined, split video and analyze it
+###
+#inputVid="D:\\GitHub\\20140725_GizmoDry_Analysis\\2014_07_28\\\
+#20140728-14_30_42-CA1_M01SS-27.MOV"
+#outputPath="D:\\GitHub\\20140725_GizmoDry_Analysis\\2014_07_28\Work"
+##roi = (900,950,800,1200)  # xmin:xmax (vertical), ymin:ymax (horizontal)
+#roi = (900,935,900,975)  # xmin:xmax (vertical), ymin:ymax (horizontal)
+#N = 10
+#print inputVid
+#print outputPath
+#videoToContactAngles(inputVid, outputPath, N, roi)
+
+# Filter video and resave it
+inputVid="D:\\GitHub\\20140725_GizmoDry_Analysis\\2014_07_28\\\
+20140728-14_30_42-CA1_M01SS-27.MOV"
+outputPath="D:\\GitHub\\20140725_GizmoDry_Analysis\\2014_07_28\Work"
+rotation = 2 # degrees
+roi = (850,1080,800,1175)  # xmin:xmax (vert, row), ymin:ymax (horz, col)
 N = 10
 print inputVid
 print outputPath
-videoToContactAngles(inputVid, outputPath, N, roi)
+#createVideo(outputPath, roi)
+videoToFilteredVideo(inputVid, outputPath, N, rotation, roi)
 
+
+#inputVid = "D:\\Dropbox\\Clients\\AFCC\\Projects\\Metal FSU\\\
+#A2 - Material Characterization\\TEST DATA\TEST CA1\\20140725_GizmoDry_Analysis\
+#\\2014_07_28\\20140728-14_30_42-CA1_M01SS-27.MOV"
+#outputPath = "D:\\Dropbox\\Clients\\AFCC\\Projects\\Metal FSU\\\
+#A2 - Material Characterization\\TEST DATA\TEST CA1\\20140725_GizmoDry_Analysis\
+#\\2014_07_28\\work"
 
 
 """
 NOTES:
+
+FOURCC CODECs
+int fourCC_code = CV_FOURCC('M','J','P','G');	// M-JPEG codec (may not 
+be reliable). 
+int fourCC_code = CV_FOURCC('P','I','M','1');	// MPEG 1 codec. 
+int fourCC_code = CV_FOURCC('D','I','V','3');	// MPEG 4.3 codec. 
+int fourCC_code = CV_FOURCC('I','2','6','3');	// H263I codec. 
+int fourCC_code = CV_FOURCC('F','L','V','1');	// FLV1 codec. 
+
+MASK LOGIC (CMPOP)
+cv2.compare(src1, src2, cmpop[, dst])
+CMP_EQ src1 is equal to src2.
+CMP_GT src1 is greater than src2.
+CMP_GE src1 is greater than or equal to src2.
+CMP_LT src1 is less than src2.
+CMP_LE src1 is less than or equal to src2.
+CMP_NE src1 is unequal to src2.
 
 Potentially useful code for making a video from images,  video it generates
 are not viable unfortunately.
